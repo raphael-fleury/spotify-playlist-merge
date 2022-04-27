fc4c6724ed3c6e4b1fec45db363924ad9f1400c8
rfleury-fcamara <54419491+raphael-fleury@users.noreply.github.com>
2022-04-27 18:37:51 -0300

    Pulling playlists from spotify

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 const config = {
     spotifyToken: process.env.SPOTIFY_TOKEN,
-    stopIfPlaylistNotFound: process.env.STOP_IF_PLAYLIST_NOT_FOUND || true
+    stopIfPlaylistNotFound: toBoolean(process.env.STOP_IF_PLAYLIST_NOT_FOUND, true)
+}
+
+function toBoolean(stringValue, defaultValue = false) {
+    if (stringValue === "true") { return true }
+    if (stringValue === "false") { return false }
+    return defaultValue;
 }
 
 export default config;
--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-import fs from "fs";
 import "dotenv/config";
+import fs from "fs";
 import config from "./config.js";
+import axios from "axios";
 
 async function main() {
     const playlistIds = getPlaylistIds();
-    console.log(playlistIds)
 
     if (!config.spotifyToken) {
         throw new Error("No OAuth token.");
+    }
+
+    for (const id of playlistIds) {
+        try {
+            const res = await getPlaylist(id);
+            console.log(`Playlist ${res.data.name} found successfully.`)
+            // console.log(res.data);
+        }
+        catch ({ response }) {
+            const { status, message } = response.data.error;
+
+            if (status == 404 && !config.stopIfPlaylistNotFound) {
+                console.error(`Playlist with id ${id} not found, skipping...`);
+                continue;
+            }
+
+            throw new Error(`${message} (Status code: ${status})`);
+        }
     }
 }
 
@@ -15,6 +33,15 @@ function getPlaylistIds() {
     return fs.readFileSync("./playlists", 'utf-8')
         .trim().replace("\r", "")
         .split("\n").filter(v => v);
+}
+
+async function getPlaylist(id) {
+    const fields = "name,owner,tracks(items,next,offset,limit)";
+    const url = `https://api.spotify.com/v1/playlists/${id}?fields=${fields}`;
+
+    return await axios.get(url, {
+        headers: { 'Authorization': `Bearer ${config.spotifyToken}` }
+    })
 }
 
 main()
