c1e83d0faf30a89de67518629cc23e81ea753b72
rfleury-fcamara <54419491+raphael-fleury@users.noreply.github.com>
2022-04-27 23:14:15 -0300

    Replacing axios with spotify-web-api-node

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import fs from "fs";
 import config from "./config.js";
-import axios from "axios";
+import SpotifyWebApi from "spotify-web-api-node";
 
 const userFields = "id,display_name"
 const trackFields = "album,artists,duration_ms,id,is_local,name";
@@ -9,13 +9,14 @@ const tracklistItemFields = `added_at,added_by,is_local,track(${trackFields})`
 const tracklistFields = `items(${tracklistItemFields}),next,offset,limit`
 const playlistFields = `id,name,owner(${userFields}),tracks(${tracklistFields})`
 
-const playlistsUrl = "https://api.spotify.com/v1/playlists";
+const spotifyApi = new SpotifyWebApi();
 
 async function main() {
     if (!config.spotifyToken) {
         throw new Error("No OAuth token.");
     }
 
+    spotifyApi.setAccessToken(config.spotifyToken);
     await getPlaylists();
 }
 
@@ -26,23 +27,20 @@ function getPlaylistIds() {
 }
 
 async function getPlaylist(id) {
-    const url = `${playlistsUrl}/${id}?fields=${playlistFields}`;
-
-    return await axios.get(url, {
-        headers: { 'Authorization': `Bearer ${config.spotifyToken}` }
-    })
+    const res = await spotifyApi.getPlaylist(id, { fields: playlistFields });
+    return res.body;
 }
 
 async function getPlaylists() {
     for (const id of getPlaylistIds()) {
         try {
-            const { data: playlist } = await getPlaylist(id);
+            const playlist = await getPlaylist(id);
             await fillPlaylistTracks(playlist);
             console.log(`Playlist "${playlist.name}" loaded successfully.`);
-            console.log(playlist.tracks.items.reverse().map(i => i.track.name))
         }
-        catch ({ response }) {
-            const { status, message } = response.data.error;
+        catch (error) {
+            console.log(error);
+            const { status, message } = error.response.data.error;
 
             if (status == 404 && !config.stopIfPlaylistNotFound) {
                 console.error(`Playlist with id ${id} not found, skipping...`);
@@ -64,11 +62,7 @@ async function fillPlaylistTracks(playlist) {
 }
 
 async function getPlaylistTracks(id, offset = 0) {
-    const url = `${playlistsUrl}/${id}/tracks?offset=${offset}`;
-
-    const { data } = await axios.get(url, {
-        headers: { 'Authorization': `Bearer ${config.spotifyToken}` }
-    })
+    const { body: data } = await spotifyApi.getPlaylistTracks(id, { offset, fields: tracklistFields });
 
     offset = data.offset;
     let { items, next, limit } = data;
@@ -80,7 +74,6 @@ async function getPlaylistTracks(id, offset = 0) {
 
     return items;
 }
-
 
 main()
     .then(() => console.log("Done!"))
