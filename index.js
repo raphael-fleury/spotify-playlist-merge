cf3b257dd14760b492f26c51c0b9ee5da208b5d2
rfleury-fcamara <54419491+raphael-fleury@users.noreply.github.com>
2022-04-27 21:29:14 -0300

    Getting all songs from playlists

--- index.js
+++ index.js
@@ -3,18 +3,43 @@ import fs from "fs";
 import config from "./config.js";
 import axios from "axios";
 
-async function main() {
-    const playlistIds = getPlaylistIds();
+const userFields = "id,display_name"
+const trackFields = "album,artists,duration_ms,id,is_local,name";
+const tracklistItemFields = `added_at,added_by,is_local,track(${trackFields})`
+const tracklistFields = `items(${tracklistItemFields}),next,offset,limit`
+const playlistFields = `id,name,owner(${userFields}),tracks(${tracklistFields})`
 
+const playlistsUrl = "https://api.spotify.com/v1/playlists";
+
+async function main() {
     if (!config.spotifyToken) {
         throw new Error("No OAuth token.");
     }
 
-    for (const id of playlistIds) {
+    await getPlaylists();
+}
+
+function getPlaylistIds() {
+    return fs.readFileSync("./playlists", 'utf-8')
+        .trim().replace("\r", "")
+        .split("\n").filter(v => v);
+}
+
+async function getPlaylist(id) {
+    const url = `${playlistsUrl}/${id}?fields=${playlistFields}`;
+
+    return await axios.get(url, {
+        headers: { 'Authorization': `Bearer ${config.spotifyToken}` }
+    })
+}
+
+async function getPlaylists() {
+    for (const id of getPlaylistIds()) {
         try {
-            const res = await getPlaylist(id);
-            console.log(`Playlist ${res.data.name} found successfully.`)
-            // console.log(res.data);
+            const { data: playlist } = await getPlaylist(id);
+            await fillPlaylistTracks(playlist);
+            console.log(`Playlist "${playlist.name}" loaded successfully.`);
+            console.log(playlist.tracks.items.reverse().map(i => i.track.name))
         }
         catch ({ response }) {
             const { status, message } = response.data.error;
@@ -29,20 +54,33 @@ async function main() {
     }
 }
 
-function getPlaylistIds() {
-    return fs.readFileSync("./playlists", 'utf-8')
-        .trim().replace("\r", "")
-        .split("\n").filter(v => v);
+async function fillPlaylistTracks(playlist) {
+    let { items, next, offset, limit } = playlist.tracks;
+
+    if (next) {
+        const tracks = await getPlaylistTracks(playlist.id, offset + limit);
+        playlist.tracks.items = [ ...items, ...tracks ];
+    }
 }
 
-async function getPlaylist(id) {
-    const fields = "name,owner,tracks(items,next,offset,limit)";
-    const url = `https://api.spotify.com/v1/playlists/${id}?fields=${fields}`;
+async function getPlaylistTracks(id, offset = 0) {
+    const url = `${playlistsUrl}/${id}/tracks?offset=${offset}`;
 
-    return await axios.get(url, {
+    const { data } = await axios.get(url, {
         headers: { 'Authorization': `Bearer ${config.spotifyToken}` }
     })
+
+    offset = data.offset;
+    let { items, next, limit } = data;
+
+    if (next) {
+        const tracks = await getPlaylistTracks(id, offset + limit);
+        items = [ ...items, ...tracks ]
+    }
+
+    return items;
 }
+
 
 main()
     .then(() => console.log("Done!"))
